6eddc3ae0fe7b07b7461bbe23d46a93ca0c7c2cb
a5chin <a5chin.origin+github@gmail.com>
2024-12-23 14:10:54 +0000

    add: test for settings

---
+++ tests/tools/test__config.py
@@ -0,0 +1,31 @@
+import pytest
+
+from tools import Settings
+from tools.config import FastAPIKwArgs
+
+
+class TestSettings:
+    """Test class for Settings."""
+
+    @pytest.mark.usefixtures("settings")
+    def test_local(self, settings: Settings) -> None:
+        """Test local settings."""
+        assert settings.IS_LOCAL
+
+    @pytest.mark.usefixtures("settings")
+    def test_fastapi_kwargs(self, settings: Settings) -> None:
+        """Test fastapi_kwargs."""
+        assert (
+            settings.fastapi_kwargs
+            == FastAPIKwArgs(
+                debug=False,
+                title="FastAPI",
+                summary=None,
+                description="",
+                version="0.1.0",
+                openapi_url="/openapi.json",
+                docs_url="/docs",
+                redoc_url="/redoc",
+                openapi_prefix="",
+            ).model_dump()
+        )
